3503c468719b11e71ae637a9948c7fa2acf7fbc6
admin <guzypaul@gmail.com>
2022-04-25 15:52:08 +0300

    HashCalculator added

--- src/main/java/by/guzypaul/blockchain/service/HashCalculator.java
+++ src/main/java/by/guzypaul/blockchain/service/HashCalculator.java
@@ -1,12 +1,14 @@
 package by.guzypaul.blockchain.service;
 
-import by.guzypaul.blockchain.entity.Block;
 import by.guzypaul.blockchain.util.StringUtil;
 
 public class HashCalculator {
-    public String calculateHash(Block block) {
+    /*public String calculateHash() {
         String calculatedhash = StringUtil.applySha256(
-                block.previousHash + Long.toString(block.timeStamp) + block.data);
+                previousHash +
+                        Long.toString(timeStamp) +
+                        data
+        );
         return calculatedhash;
-    }
+    }*/
 }
