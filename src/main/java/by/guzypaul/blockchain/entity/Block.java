c40feb2d3444963ef90d03d1d47056bbdef8d1cd
admin <guzypaul@gmail.com>
2022-04-28 11:15:36 +0300

    method isChainValid added

--- src/main/java/by/guzypaul/blockchain/entity/Block.java
+++ src/main/java/by/guzypaul/blockchain/entity/Block.java
@@ -9,6 +9,7 @@ public class Block {
     public String previousHash;
     private String data; //our data will be a simple message.
     private long timeStamp; //as number of milliseconds since 1/1/1970.
+    private int nonce;
 
     //Block Constructor.
     public Block(String data,String previousHash ) {
@@ -22,5 +23,14 @@ public class Block {
         String calculatedhash = StringUtil.applySha256(
                 previousHash + Long.toString(timeStamp) + data);
         return calculatedhash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
